02f71313f7440700fc3560d6613e5763586ff2f2
Greg Teixeira <greg.teixeira123@gmail.com>
2021-03-24 10:53:57 -0300

    feat: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import mainRoutes from 'routes'
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	)
+const App = () => {
+	const routes = useRoutes(mainRoutes)
+	return routes
 }
 
 export default App
