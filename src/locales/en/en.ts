d70bc0078e50378e3939a234f2bf1758a23d5f39
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-14 19:06:40 -0300

    feat: add english locales

---
+++ src/locales/en/en.ts
@@ -0,0 +1,15 @@
+export const enLocales = {
+	'home.title': 'Home',
+	'home.link': 'Go to second page',
+
+	'secondPage.title': 'Second Page',
+	'secondPage.link': 'Go to Home',
+
+	'counter.increment': 'Increment',
+	'counter.decrement': 'Decrement',
+	'counter.count': 'Count: {value}',
+
+	changeLanguage: 'Change language',
+	en: 'English',
+	'pt-BR': 'Portuguese'
+}
