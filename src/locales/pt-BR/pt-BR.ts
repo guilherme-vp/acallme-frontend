7dbbe36d490b8e6a744f4cb527fa0e41e5d34afc
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-14 19:06:49 -0300

    feat: add brazilian portuguese locales

---
+++ src/locales/pt-BR/pt-BR.ts
@@ -0,0 +1,17 @@
+import { ILocale } from '../ILocale'
+
+export const ptBrLocales: ILocale = {
+	'home.title': 'Casa',
+	'home.link': 'Ir para a segunda página',
+
+	'secondPage.title': 'Hoola',
+	'secondPage.link': 'Ir para a Home',
+
+	'counter.increment': 'Incrementar',
+	'counter.decrement': 'Decrementar',
+	'counter.count': 'Contagem atual: {value}',
+
+	changeLanguage: 'Mudar língua',
+	en: 'Inglês',
+	'pt-BR': 'Português'
+}
