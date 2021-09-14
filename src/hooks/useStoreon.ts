a49223db4b831adbbdc2b38bbdc40896023a0128
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-14 19:06:24 -0300

    feat: add useStoreon hook

---
+++ src/hooks/useStoreon.ts
@@ -0,0 +1,5 @@
+import { useStoreon as useStateStoreon } from 'storeon/react'
+import { AppState, AppEvents } from '../state/store'
+
+export const useStoreon = (...keys: (keyof AppState)[]) =>
+	useStateStoreon<AppState, AppEvents>(...keys)
