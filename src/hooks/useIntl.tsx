0d8d31cd7e610e939588dc83c50f3982997f46d8
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-14 19:06:32 -0300

    feat: add intl messages hook

---
+++ src/hooks/useIntl.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from 'react'
+import { useIntl as useBaseIntl, IntlShape as Shape } from 'react-intl'
+
+import { enLocales } from '../locales/en/en'
+import { ILocale } from '../locales/ILocale'
+
+interface MessageDescriptor {
+	id: keyof ILocale
+	description?: object
+}
+
+export interface IntlShape extends Omit<Shape, 'formatMessage'> {
+	formatMessage(
+		descriptor: MessageDescriptor,
+		values?: Parameters<Shape['formatMessage']>[1]
+	): string
+	formatMessage(
+		descriptor: MessageDescriptor,
+		values?: Parameters<Shape['formatMessage']>[1]
+	): ReactNode
+}
+
+export const useIntl = (): IntlShape => {
+	const { formatMessage, ...intl } = useBaseIntl()
+	return {
+		...intl,
+		// @ts-ignore
+		formatMessage: (
+			descriptor: MessageDescriptor,
+			values?: Parameters<IntlShape['formatMessage']>[1]
+		): ReturnType<IntlShape['formatMessage']> =>
+			formatMessage(
+				{ ...descriptor, defaultMessage: enLocales[descriptor.id] },
+				{ bold: str => <strong>{str}</strong>, linebreak: <br />, ...values }
+			)
+	}
+}
+
+export type { ILocale }
+
+export default useIntl
