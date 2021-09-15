1fd2e17feeb3c453a1b99885ea6e77f2feae3c0b
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-15 19:43:54 -0300

    refactor: change language picker padding

---
+++ src/components/LanguagePicker/LanguagePicker.styled.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { MenuItem } from '@material-ui/core'
+
+export const Item = styled(MenuItem)`
+	padding: 12px 20px !important;
+`
--- src/components/LanguagePicker/LanguagePicker.tsx
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -1,29 +1,33 @@
 import React from 'react'
-import { MenuItem, Select } from '@material-ui/core'
+import { Select } from '@material-ui/core'
 
 import { useIntl, useStoreon } from '../../hooks'
-import { AvailableLanguages } from '../../locales'
+import { AvailableLanguages, Locales } from '../../locales'
+
+import { Item } from './LanguagePicker.styled'
 
 export const LanguagePicker = () => {
 	const { dispatch, language } = useStoreon('language')
 	const intl = useIntl()
 
+	const handleChange = (event: React.ChangeEvent<{ value: Locales }>) => {
+		const newLanguage = event.target.value
+
+		dispatch('language/change', newLanguage)
+	}
+
 	return (
-		<Select onChange={e => dispatch('language/change', e.target.value)} value={language}>
-			<MenuItem
-				disableRipple
-				value={AvailableLanguages.ENGLISH}
-				selected={language === 'en'}
-			>
+		<Select onChange={handleChange} value={language} variant="outlined" sx={{ padding: 0 }}>
+			<Item disableRipple value={AvailableLanguages.ENGLISH} selected={language === 'en'}>
 				{intl.formatMessage({ id: 'en' })}
-			</MenuItem>
-			<MenuItem
+			</Item>
+			<Item
 				disableRipple
 				value={AvailableLanguages.PORTUGUESE}
 				selected={language === 'pt-BR'}
 			>
 				{intl.formatMessage({ id: 'pt-BR' })}
-			</MenuItem>
+			</Item>
 		</Select>
 	)
 }
