275d8d6ef8d5479dd9f934188d64309af3425492
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-15 02:47:07 -0300

    feat: add material theme provider

--- src/styles/Wrapper.styles.tsx
+++ src/styles/Wrapper.styles.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { StyledEngineProvider } from '@material-ui/styled-engine'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import { ThemeProvider } from 'styled-components'
+
 import { theme } from './theme'
+import { materialTheme } from './material-theme'
 import { GlobalStyle } from './Global.styles'
 
 export const StylesWrapper: React.FC = ({ children }) => (
 	<StyledEngineProvider injectFirst>
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			{children}
-		</ThemeProvider>
+		<MuiThemeProvider theme={materialTheme}>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				{children}
+			</ThemeProvider>
+		</MuiThemeProvider>
 	</StyledEngineProvider>
 )
