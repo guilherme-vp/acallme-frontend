fff97ad9a0165c429fc965b640b0e8210c4d04e4
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-15 14:45:10 -0300

    feat: add about section labels

--- src/locales/en/en.ts
+++ src/locales/en/en.ts
@@ -10,10 +10,20 @@ export const enLocales: ILocale = {
 	'home.section.initial.title': 'Offering The Best For Your Anxiety',
 	'home.section.initial.description':
 		'Find the ideal specialist and treatment in just a few clicks. Start now to treat yourself via Teleconsultation, distance is not a problem.',
-	'home.section.initial.apointment': 'Book an Apointment',
+	'home.section.initial.appointment': 'Book an Appointment',
 	'home.section.initial.box.patients': 'Patients Every Day',
 	'home.section.initial.box.years': 'Years Experience',
 	'home.section.initial.box.records': 'Diagnosis',
+
+	'home.section.about.title': 'Taking Care of You and Your Health Every Time',
+	'home.section.about.description':
+		'We and our professionals will be ready to assist you and give you all the support you need to carry out your service.',
+	'home.section.about.description.2': 'What we offer:',
+	'home.section.about.bullet.1': 'Expert Professionals',
+	'home.section.about.bullet.2': 'The best of Teleconsultation',
+	'home.section.about.bullet.3': 'Digital Safety',
+	'home.section.about.bullet.4': 'Help Center 24x7',
+	'home.section.about.learnMore': 'Learn More',
 
 	// Languages
 	en: 'English',
--- src/locales/pt-BR/pt-BR.ts
+++ src/locales/pt-BR/pt-BR.ts
@@ -8,10 +8,20 @@ export const ptBrLocales = {
 	'home.section.initial.title': 'Oferecendo o Melhor Para a Sua Ansiedade',
 	'home.section.initial.description':
 		'Encontre o especialista e o tratamento ideais em poucos cliques. Comece agora a se tratar via Teleconsulta, a distância não é um problema.',
-	'home.section.initial.apointment': 'Agende uma Consulta',
+	'home.section.initial.appointment': 'Agende uma Consulta',
 	'home.section.initial.box.patients': 'Pacientes Todos os Dias',
 	'home.section.initial.box.years': 'Anos de Experiência',
 	'home.section.initial.box.records': 'Prontuários Realizados',
+
+	'home.section.about.title': 'Cuidando de Você e da Sua Saúde a Todo Tempo',
+	'home.section.about.description':
+		'Nós e nossos profissionais estaremos prontos para te atender e dar todo o suporte necessário para você realizar seu atendimento.',
+	'home.section.about.description.2': 'O quê nós lhe oferecemos:',
+	'home.section.about.bullet.1': 'Profissionais Especializados',
+	'home.section.about.bullet.2': 'O Melhor da Telemedicina',
+	'home.section.about.bullet.3': 'Segurança Digital',
+	'home.section.about.bullet.4': 'Help Center 24x7',
+	'home.section.about.learnMore': 'Saiba Mais',
 
 	en: 'Inglês',
 	'pt-BR': 'Português'
