2ce2f6da7adeece9329cfd05b8d54e1af47bb604
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-15 17:41:34 -0300

    feat: add services section to home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { LanguagePicker } from 'components/LanguagePicker'
 import { Container } from './Home.styled'
-import { Initial, About } from './sections'
+import { Initial, About, Services } from './sections'
 
 export const Home = () => (
 	<Container>
 		<Initial />
 		<About />
+		<Services />
 		<LanguagePicker />
 	</Container>
 )
