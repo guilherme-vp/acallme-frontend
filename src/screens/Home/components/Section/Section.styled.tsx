bb95baa43e2d69cae0a94daac3450be3cd70014e
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-15 14:46:08 -0300

    fix: increase margin between sections

--- src/screens/Home/components/Section/Section.styled.tsx
+++ src/screens/Home/components/Section/Section.styled.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const FullPageSection = styled.section`
-	margin: 12px 0;
+	margin: 12px 0 200px;
 	flex-grow: 1;
 	display: flex;
 	justify-content: center;
