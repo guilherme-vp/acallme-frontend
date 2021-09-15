e65ebd7e3d3c9b4bfd0676d1158ae63424953822
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-15 14:47:14 -0300

    feat: add about section and language picker component

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { LanguagePicker } from 'components/LanguagePicker'
 import { Container } from './Home.styled'
-import { Initial } from './sections'
+import { Initial, About } from './sections'
 
 export const Home = () => (
 	<Container>
 		<Initial />
+		<About />
+		<LanguagePicker />
 	</Container>
 )
 
