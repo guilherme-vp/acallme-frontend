8c1b065fc588b5de218df5a63d30cd0511dc81b2
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-15 02:49:40 -0300

    feat: add home section locale

--- src/locales/en/en.ts
+++ src/locales/en/en.ts
@@ -2,7 +2,20 @@ import { ILocale } from '../ILocale'
 
 export const enLocales: ILocale = {
 	'home.title': 'Home',
+	'home.header.home': 'Home',
+	'home.header.about': 'About',
+	'home.header.services': 'Services',
+	'home.header.contact': 'Contact Us',
 
+	'home.section.initial.title': 'Offering The Best For Your Anxiety',
+	'home.section.initial.description':
+		'Find the ideal specialist and treatment in just a few clicks. Start now to treat yourself via Teleconsultation, distance is not a problem.',
+	'home.section.initial.apointment': 'Book an Apointment',
+	'home.section.initial.box.patients': 'Patients Every Day',
+	'home.section.initial.box.years': 'Years Experience',
+	'home.section.initial.box.records': 'Diagnosis',
+
+	// Languages
 	en: 'English',
 	'pt-BR': 'Portuguese'
 }
--- src/locales/pt-BR/pt-BR.ts
+++ src/locales/pt-BR/pt-BR.ts
@@ -1,5 +1,17 @@
 export const ptBrLocales = {
 	'home.title': 'Home',
+	'home.header.home': 'Home',
+	'home.header.about': 'Sobre',
+	'home.header.services': 'Serviços',
+	'home.header.contact': 'Entre em Contato',
+
+	'home.section.initial.title': 'Oferecendo o Melhor Para a Sua Ansiedade',
+	'home.section.initial.description':
+		'Encontre o especialista e o tratamento ideais em poucos cliques. Comece agora a se tratar via Teleconsulta, a distância não é um problema.',
+	'home.section.initial.apointment': 'Agende uma Consulta',
+	'home.section.initial.box.patients': 'Pacientes Todos os Dias',
+	'home.section.initial.box.years': 'Anos de Experiência',
+	'home.section.initial.box.records': 'Prontuários Realizados',
 
 	en: 'Inglês',
 	'pt-BR': 'Português'
