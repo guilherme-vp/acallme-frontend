95d5dba24ad941b01bb01909027a3a85774a66ad
YDan08 <dasyamashita@gmail.com>
2021-10-04 08:08:02 -0300

    Criar pagina login

---
+++ src/screens/Login/Login.styled.tsx
@@ -0,0 +1,114 @@
+import styled from 'styled-components'
+
+export const DivLogin = styled.div`
+	width: 100vw;
+	height: 80vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+export const ContainerLogin = styled.div`
+	width: 100%;
+	height: 100%;
+	border-radius: 10px;
+	padding: 0 24px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	a {
+		text-decoration: none;
+		color: #0c99ac;
+	}
+	@media (max-width: 1280px) {
+		#imagem {
+			display: none;
+		}
+	}
+
+	@media (max-width: 960px) {
+		width: 100%;
+		height: 100%;
+		border: none;
+		border-radius: 0;
+		padding: 0 16px;
+		body {
+			overflow-x: hidden;
+		}
+	}
+`
+
+export const Dados = styled.div`
+	position: relative;
+	justify-content: space-around;
+	align-items: center;
+
+	input {
+		width: 100%;
+		border: 2px solid #aaa;
+		background-color: #ffffffb0;
+		outline: none;
+		border-radius: 4px;
+		padding: 8px 40px;
+		transition: 0.3s;
+		:focus {
+			border-color: #0c99ac;
+			background-color: #f3f1f1;
+		}
+	}
+
+	.icon {
+		position: absolute;
+		top: 10px;
+		left: 0px;
+		padding: 0px 14px;
+	}
+	.iconEye {
+		position: absolute;
+		top: 10px;
+		right: 0px;
+		padding: 0px 14px;
+		svg {
+			cursor: pointer;
+		}
+	}
+	@media (max-width: 1280px) {
+		.icon {
+			padding: 0px 8px;
+		}
+		input {
+			padding: 8px 24px;
+		}
+	}
+`
+
+export const DivInfo = styled.div`
+	width: 100%;
+	h1 {
+		margin-bottom: 16px;
+	}
+	p {
+		text-align: justify;
+	}
+`
+
+export const DivButtonDados = styled.div`
+	display: flex;
+	width: 80%;
+	input,
+	button {
+		background-color: #ffffffb0;
+		padding: 8px;
+		width: 40%;
+		border-radius: 16px;
+		border: 1px solid black;
+		color: black;
+		margin-right: 16px;
+		cursor: pointer;
+		:hover {
+			background-color: #0c99ac;
+			color: white;
+		}
+	}
+`
