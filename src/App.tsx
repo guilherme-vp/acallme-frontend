b65548da1ad0d420861c22c0d64f887ce00c09fe
YDan08 <dasyamashita@gmail.com>
2021-10-04 08:03:11 -0300

    Criar pagina login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
-import { Home } from './screens/Home'
+//import { Home } from './screens/Home'
+import Login from './screens/Login/Login'
 import { DefaultLayout } from './layouts/default'
 
 const App = () => (
 	<DefaultLayout>
-		<Home />
+		<Login/>
 	</DefaultLayout>
 )
 
---
+++ src/screens/Login/Login.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react'
+
+import { Grid } from '@material-ui/core'
+import { BiEnvelope } from 'react-icons/bi'
+import { AiOutlineLock, AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
+import { DivLogin, ContainerLogin, Dados, DivButtonDados, DivInfo } from './Login.styled'
+
+const Login = () => {
+	const [ativo, setAtivo] = useState(false)
+
+	function olhar() {
+		if (ativo === true) {
+			setAtivo(false)
+		} else {
+			setAtivo(true)
+		}
+	}
+
+	return (
+		<DivLogin>
+			<ContainerLogin>
+				<Grid container justifyContent="center" alignContent="center">
+					<Grid
+						container
+						item
+						lg={7}
+						justifyContent="center"
+						alignItems="center"
+						id="imagem"
+					>
+						<Grid item>
+							<img
+								src="https://i1.wp.com/sensorweb.com.br/wp-content/uploads/2019/08/header_25_09_19.png?fit=845%2C684&ssl=1"
+								alt="medico e paciente"
+								style={{ width: '500px' }}
+							/>
+						</Grid>
+					</Grid>
+
+					<Grid
+						container
+						item
+						xs={12}
+						md={12}
+						lg={5}
+						spacing={5}
+						justifyContent="center"
+						alignItems="center"
+					>
+						<Grid item>
+							<DivInfo>
+								<h1>Bem-Vindo !</h1>
+								<p>
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque
+									consequuntur molestiae exercitationem corporis ullam necessitatibus eum
+									voluptatum fugit, nobis ipsa quae. Repellendus corrupti placeat nam ipsam!
+									Velit, sapiente ullam! Officiis.
+								</p>
+							</DivInfo>
+						</Grid>
+
+						<Grid container item>
+							<form method="get">
+								<Grid container item spacing={2}>
+									<Grid item xs={12}>
+										<Dados>
+											<BiEnvelope className="icon" />
+											<input type="email" placeholder="Email" name="email" required />
+										</Dados>
+									</Grid>
+
+									<Grid item xs={12}>
+										<Dados>
+											<AiOutlineLock className="icon" />
+											<input
+												type={ativo ? 'text' : 'password'}
+												placeholder="Senha"
+												name="senha"
+												required
+											/>
+											<div onClick={olhar} className="iconEye">
+												{ativo ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+											</div>
+										</Dados>
+									</Grid>
+
+									<Grid item xs={12}>
+										<a href="./">Esqueceu a senha?</a>
+									</Grid>
+
+									<Grid item xs={12}>
+										<DivButtonDados>
+											<input type="submit" name="Login" />
+											<button>Criar Conta</button>
+										</DivButtonDados>
+									</Grid>
+								</Grid>
+							</form>
+						</Grid>
+					</Grid>
+				</Grid>
+			</ContainerLogin>
+		</DivLogin>
+	)
+}
+
+export default Login
