02fff9a427c9d224b2bfb28db3da77c369379118
YDan08 <dasyamashita@gmail.com>
2021-10-07 08:44:38 -0300

    adicionar regra no eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import Home from 'screens/Home'
 import Login from 'screens/Login/Login'
 import { DefaultLayout } from './layouts/default'
 
 const App = () => (
-	<DefaultLayout>
-		<Login />
-	</DefaultLayout>
+	<BrowserRouter>
+		<Switch>
+			<DefaultLayout>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+			</DefaultLayout>
+		</Switch>
+	</BrowserRouter>
 )
 
 export default App
