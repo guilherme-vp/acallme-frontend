7c5093da000d8a8b45c21b122a64e985d5a3e5b8
Guilherme Vieira <guivpw68@gmail.com>
2021-10-12 00:36:11 -0300

    feat: add wizard form

--- src/screens/SignUp/SignUp.tsx
+++ src/screens/SignUp/SignUp.tsx
@@ -1,127 +1,135 @@
 import React, { useState } from 'react'
-import { Grid, Button, IconButton } from '@mui/material'
-import { AiOutlineLock, AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
-import { BiEnvelope } from 'react-icons/bi'
-import { BsPerson } from 'react-icons/bs'
-import { FaTransgenderAlt, FaBaby } from 'react-icons/fa'
-import { HiOutlineIdentification } from 'react-icons/hi'
-import { Link } from 'react-router-dom'
+import { Grid, Button, Typography } from '@mui/material'
+import { useTheme } from 'styled-components'
+import { FormProvider, useForm } from 'react-hook-form'
+import iziToast from 'izitoast'
+import { LoadingButton } from '@mui/lab'
 import { useIntl } from '../../hooks'
-import { ContainerSign, Dados, DivSubmit } from './SignUp.styled'
-import telefone from '../../assets/icons/telefone.svg'
+import { RolesEnum, GenderEnum } from '../../services/entities'
+import { Role } from './containers/Role'
+import { PersonalInformation } from './containers/PersonalInformation'
+import { capitalizeLetter } from '../../utils/capitalize-letter'
+
+export interface PersonalForm {
+	name: string
+	photo?: File
+	birth: Date | null
+	phone: string
+	gender: GenderEnum
+}
+
+export interface AccountForm {
+	email: string
+	password: string
+	cpf: string
+}
 
 export const SignUp = () => {
 	const intl = useIntl()
-	const [active, setActive] = useState(false)
+	const [step, setStep] = useState(0)
+	const [chosen, setChosen] = useState<RolesEnum>()
+	const theme = useTheme()
+	const personalMethods = useForm<PersonalForm>({
+		mode: 'all',
+		defaultValues: {
+			birth: null
+		}
+	})
+	const accountMethods = useForm<AccountForm>({
+		mode: 'all'
+	})
 
-	function look() {
-		if (active === true) {
-			setActive(false)
-		} else {
-			setActive(true)
+	const handleChoseRole = (role: RolesEnum) => {
+		setChosen(role)
+	}
+
+	const handlePreviousStep = () => {
+		if (step !== 0) {
+			setStep(step - 1)
+		}
+	}
+
+	const handleNextStep = () => {
+		if (step === 0) {
+			setStep(step + 1)
+		} else if (step === 1) {
+			if (personalMethods.formState.isValid) {
+				setStep(step + 1)
+			} else {
+				iziToast.error({
+					message: intl.formatMessage({ id: 'signup.form.personal.required' })
+				})
+			}
+		} else if (step === 2) {
+			if (accountMethods.formState.isValid) {
+				setStep(step + 1)
+			} else {
+				iziToast.error({
+					message: intl.formatMessage({ id: 'signup.form.personal.required' })
+				})
+			}
+		} else if (step === 3) {
 		}
 	}
 
 	return (
-		<ContainerSign>
-			<Grid container spacing={2}>
-				<Grid item lg={8} id="imagem">
-					<img
-						src="https://www.epays.com.br/wp-content/uploads/2020/09/img2-1024x683.png"
-						alt="person-with-notebook"
-					/>
-				</Grid>
-
-				<Grid
-					container
-					item
-					xs={12}
-					lg={4}
-					spacing={2}
-					justifyContent="center"
-					alignItems="center"
-				>
-					<Grid item xs={12}>
-						<h1 style={{ textAlign: 'center', marginBottom: '16px', color: '#0e63f4' }}>
-							{intl.formatMessage({ id: 'signupPatient.title' })}
-						</h1>
-
-						<form>
-							<Dados>
-								<input
-									type="text"
-									name="name"
-									placeholder={intl.formatMessage({ id: 'signupPatient.option.name' })}
-									required
-								/>
-								<BsPerson className="icon" />
-							</Dados>
-
-							<Dados>
-								<input type="email" name="email" placeholder="Email" required />
-								<BiEnvelope className="icon" />
-							</Dados>
-
-							<Dados>
-								<input
-									type={active ? 'text' : 'password'}
-									placeholder={intl.formatMessage({ id: 'signupPatient.option.senha' })}
-									name="password"
-									required
-								/>
-								<AiOutlineLock className="icon" />
-								<IconButton onClick={look} className="iconEye">
-									{active ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
-								</IconButton>
-							</Dados>
-
-							<Dados>
-								<input type="date" id="dtNascimento" required />
-								<FaBaby className="icon" />
-							</Dados>
-
-							<Dados>
-								<select name="gender" id="genero" required>
-									<option value="h">
-										{intl.formatMessage({ id: 'signupPatient.option.genre.man' })}
-									</option>
-									<option value="m">
-										{intl.formatMessage({ id: 'signupPatient.option.genre.woman' })}
-									</option>
-									<option value="nb">
-										{intl.formatMessage({ id: 'signupPatient.option.genre.nb' })}
-									</option>
-								</select>
-								<FaTransgenderAlt className="icon" />
-							</Dados>
-
-							<Dados>
-								<HiOutlineIdentification className="icon" />
-								<input type="number" name="cpf" placeholder="CPF" />
-							</Dados>
-
-							<Dados>
-								<img src={telefone} alt="telefone" className="icon" />
-								<input
-									type="number"
-									name="cellphone"
-									placeholder={intl.formatMessage({ id: 'signupPatient.option.cellphone' })}
-								/>
-							</Dados>
-
-							<DivSubmit>
-								<Button type="submit">
-									{intl.formatMessage({ id: 'signupPatient.submit' })}
-								</Button>
-								<Link to="/login">
-									{intl.formatMessage({ id: 'signupPatient.a.login' })}
-								</Link>
-							</DivSubmit>
-						</form>
+		<Grid container item xs={12}>
+			<Grid item xs={12} sx={{ textAlign: 'center' }}>
+				{step !== 3 && (
+					<Typography
+						align="center"
+						variant="display2"
+						sx={{ color: theme.colors.alternative }}
+					>
+						{/* @ts-ignore */}
+						{intl.formatMessage({ id: `signup.title.${step}` })}
+					</Typography>
+				)}
+			</Grid>
+			{step === 0 ? (
+				<Role roleChosen={chosen} handleClick={handleChoseRole} />
+			) : step === 1 ? (
+				<FormProvider {...personalMethods}>
+					<PersonalInformation />
+				</FormProvider>
+			) : (
+				<div />
+			)}
+			<Grid container item justifyContent="center" alignItems="center" mt={5}>
+				{step !== 0 && (
+					<Grid item mr={2}>
+						<Button onClick={() => handlePreviousStep()}>
+							{capitalizeLetter(
+								intl.formatMessage({
+									id: 'goBack'
+								})
+							)}
+						</Button>
 					</Grid>
+				)}
+				<Grid>
+					<LoadingButton
+						disabled={
+							step === 0
+								? !chosen
+								: step === 1
+								? !personalMethods.formState.isValid
+								: step === 2
+								? !accountMethods.formState.isValid
+								: !chosen
+						}
+						onClick={() => handleNextStep()}
+						variant="contained"
+					>
+						{capitalizeLetter(
+							intl.formatMessage({
+								id: step !== 2 ? 'signup.choose.next' : 'signup.choose.next.last'
+							})
+						)}
+					</LoadingButton>
 				</Grid>
 			</Grid>
-		</ContainerSign>
+		</Grid>
 	)
 }
 
