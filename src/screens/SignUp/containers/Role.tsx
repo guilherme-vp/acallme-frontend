1428adf7bda07b8f07cd99ca1ae677b4f9de4582
Guilherme Vieira <guivpw68@gmail.com>
2021-10-12 00:35:54 -0300

    feat: add chose role step

---
+++ src/screens/SignUp/containers/Role.tsx
@@ -0,0 +1,51 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+import { RolesEnum } from 'services/entities'
+import { CardButton } from '../../../components/CardButton'
+import { useIntl } from '../../../hooks'
+import patient from '../../../assets/images/signup-patient.png'
+import specialist from '../../../assets/images/signup-specialist.png'
+import { ChosenImage } from '../SignUp.styled'
+
+export interface RoleProps {
+	roleChosen?: RolesEnum
+	handleClick: (role: RolesEnum) => void
+}
+
+export const Role = ({ roleChosen, handleClick }: RoleProps) => {
+	const intl = useIntl()
+
+	return (
+		<Grid container justifyContent="center" mt={5} spacing={2}>
+			<Grid container item xs={9} md={5}>
+				<CardButton
+					fullWidth
+					variant="outlined"
+					selected={roleChosen === RolesEnum.Patient}
+					onClick={() => handleClick(RolesEnum.Patient)}
+				>
+					<ChosenImage src={patient} alt="patient-option" />
+					<Typography variant="h4">
+						{intl.formatMessage({ id: 'patient' }).toUpperCase()}
+					</Typography>
+				</CardButton>
+			</Grid>
+			<Grid container item xs={9} md={5}>
+				<CardButton
+					fullWidth
+					variant="outlined"
+					color="success"
+					selected={roleChosen === RolesEnum.Specialist}
+					onClick={() => handleClick(RolesEnum.Specialist)}
+				>
+					<ChosenImage src={specialist} alt="specialist-option" />
+					<Typography variant="h4">
+						{intl.formatMessage({ id: 'specialist' }).toUpperCase()}
+					</Typography>
+				</CardButton>
+			</Grid>
+		</Grid>
+	)
+}
+
+export default Role
