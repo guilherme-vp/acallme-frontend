d9f15d050fbc4807dbd595908d2822e93045b867
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-12 16:10:02 -0300

    refactor: remove language picker from footer to header

--- src/parts/Footer/Footer.default.tsx
+++ src/parts/Footer/Footer.default.tsx
@@ -8,14 +8,14 @@ export const Footer = () => {
 
 	return (
 		<footer>
-			<Grid container spacing={1} mt={2} justifyContent="flex-end">
-				<Grid item xs={12}>
+			<Grid container spacing={1} my={2} flexDirection="column" alignItems="center">
+				<Grid item>
 					<Typography variant="h4" color="textSecondary" fontWeight={500}>
 						{intl.formatMessage({ id: 'home.footer.language' })}
 					</Typography>
 				</Grid>
 
-				<Grid item xs={12}>
+				<Grid item>
 					<LanguagePicker />
 				</Grid>
 			</Grid>
