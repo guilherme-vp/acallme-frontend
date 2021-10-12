5e5e8614ca1e2a0be6144b9058722237b32db7f9
Guilherme Vieira <guivpw68@gmail.com>
2021-10-12 00:37:19 -0300

    feat: add izitoast config

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useMediaQuery } from '@mui/material'
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import iziToast from 'izitoast'
 import { Home } from 'screens/Home'
 import { Login } from 'screens/Login'
 import { SignUp } from 'screens/SignUp'
 import { HomeLayout } from './layouts/Home'
 import { AuthLayout } from './layouts/Auth'
+import 'izitoast/dist/css/iziToast.min.css'
 
-const App = () => (
-	<Router>
-		<Switch>
-			<Route path="/" exact>
-				<HomeLayout>
-					<Home />
-				</HomeLayout>
-			</Route>
-			<Route path="/login">
-				<AuthLayout>
-					<Login />
-				</AuthLayout>
-			</Route>
-			<Route path="/signup">
-				<AuthLayout>
-					<SignUp />
-				</AuthLayout>
-			</Route>
-		</Switch>
-	</Router>
-)
+iziToast.settings({
+	position: 'bottomLeft',
+	maxWidth: 400
+})
+
+const App = () => {
+	const smDown = useMediaQuery('(max-width: 600px)')
+
+	useEffect(() => {
+		if (smDown) {
+			iziToast.settings({
+				position: 'topLeft'
+			})
+		} else {
+			iziToast.settings({
+				position: 'bottomLeft',
+				maxWidth: 400
+			})
+		}
+	}, [smDown])
+
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<HomeLayout>
+						<Home />
+					</HomeLayout>
+				</Route>
+				<Route path="/login">
+					<AuthLayout>
+						<Login />
+					</AuthLayout>
+				</Route>
+				<Route path="/signup">
+					<AuthLayout>
+						<SignUp />
+					</AuthLayout>
+				</Route>
+			</Switch>
+		</Router>
+	)
+}
 
 export default App
