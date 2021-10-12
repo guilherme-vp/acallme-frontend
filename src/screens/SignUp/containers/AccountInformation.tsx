c481cabc51ab52c0d09079527782c0ba1e018eb2
Guilherme Vieira <guivpw68@gmail.com>
2021-10-12 01:09:38 -0300

    feat: add account information step

---
+++ src/screens/SignUp/containers/AccountInformation.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Grid, TextField } from '@mui/material'
+import { useFormContext } from 'react-hook-form'
+import { useIntl } from '../../../hooks'
+import type { AccountForm } from '../SignUp'
+import { cpfRegex, createStringRequirements, emailRegex } from '../../../utils/regexs'
+import { PasswordInput } from '../../../components/PasswordInput'
+
+const passwordRegex = createStringRequirements()
+
+export const AccountInformation = () => {
+	const intl = useIntl()
+	const {
+		register,
+		formState: { errors },
+		watch
+	} = useFormContext<AccountForm>()
+	const allFields = watch()
+
+	return (
+		<Grid container item mt={5} justifyContent="center">
+			<Grid container item flexDirection="column" spacing={2} xs={12} md={7}>
+				<Grid item>
+					<TextField
+						{...register('email', {
+							required: true,
+							pattern: emailRegex
+						})}
+						fullWidth
+						label={intl.formatMessage({ id: 'email' })}
+						variant="outlined"
+						required
+						error={!!errors.email}
+						name="email"
+						type="email"
+						autoComplete="email"
+					/>
+				</Grid>
+				<Grid item>
+					<PasswordInput
+						{...register('password', {
+							required: true,
+							pattern: passwordRegex
+						})}
+						helperText="Sua senha deve conter no mínimo 6 caracteres, uma letra maiúscula, uma minúscula e um número"
+						fullWidth
+						required
+						error={!!errors.password}
+						label={intl.formatMessage({ id: 'password' })}
+						isCorrect={allFields.password ? !errors.password : true}
+					/>
+				</Grid>
+				<Grid item>
+					<TextField
+						{...register('cpf', {
+							required: true,
+							pattern: cpfRegex
+						})}
+						fullWidth
+						label={intl.formatMessage({ id: 'cpf' })}
+						variant="outlined"
+						required
+						error={!!errors.cpf}
+						placeholder="111.111.111-11"
+						name="cpf"
+						type="text"
+						autoComplete="cpf"
+					/>
+				</Grid>
+			</Grid>
+		</Grid>
+	)
+}
+
+export default AccountInformation
