3807577ec7b0a02d916acc436cab093171027d37
Guilherme Vieira <guivpw68@gmail.com>
2021-10-13 02:53:59 -0300

    feat: add patient login

--- src/services/api/patient/queries.ts
+++ src/services/api/patient/queries.ts
@@ -2,6 +2,16 @@ import { Patient } from 'services/entities'
 
 import { nodeApi } from '../client'
 
+interface LoginDto {
+	username: string
+	password: string
+}
+
+interface LoginResponse {
+	user: Patient
+	token: string
+}
+
 export async function fetchPatientById(id: number): Promise<Patient> {
 	const { data } = await nodeApi.get(`patients/:${id}`)
 
@@ -9,13 +19,19 @@ export async function fetchPatientById(id: number): Promise<Patient> {
 }
 
 export async function fetchPatients(): Promise<Patient[]> {
-	const { data } = await nodeApi.get(`patients`)
+	const { data } = await nodeApi.get('patients')
 
 	return data as Patient[]
 }
 
 export async function createPatient(): Promise<Patient> {
-	const { data } = await nodeApi.get(`patients`)
+	const { data } = await nodeApi.get('patients')
 
 	return data as Patient
+}
+
+export async function loginPatient(input: LoginDto): Promise<LoginResponse> {
+	const { data } = await nodeApi.post('patients/login', input)
+
+	return data as LoginResponse
 }
