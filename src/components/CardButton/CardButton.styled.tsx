ea2e54aa6472d93f6ea66394588bb4f2307dedcd
Guilherme Vieira <guivpw68@gmail.com>
2021-10-13 02:29:48 -0300

    fix: improve responsive height

--- src/components/CardButton/CardButton.styled.tsx
+++ src/components/CardButton/CardButton.styled.tsx
@@ -1,8 +1,11 @@
 import { Button } from '@mui/material'
 import styled from 'styled-components'
 
-export const CardButton = styled(Button).withConfig<{ selected?: boolean }>({
-	shouldForwardProp: props => !['selected'].includes(props)
+export const CardButton = styled(Button).withConfig<{
+	selected?: boolean
+	size?: 'small' | 'large'
+}>({
+	shouldForwardProp: props => !['selected', 'size'].includes(props)
 })`
 	box-sizing: border-box;
 	display: flex;
@@ -10,14 +13,18 @@ export const CardButton = styled(Button).withConfig<{ selected?: boolean }>({
 	align-items: center;
 	justify-content: space-between;
 	border-radius: 8px;
-	padding: 40px 20px !important;
-	height: 300px;
+	padding: ${props => (props.size === 'small' ? '30px 20px' : '40px 20px')}!important;
+	height: 100%;
 	color: ${props => props.theme.colors.alternative} !important;
 	border: 1px solid
 		${props => (props.selected ? props.theme.border.colored : props.theme.border.darker)} !important;
 
 	img {
-		width: 100%;
+		width: 150px;
 		margin-bottom: 16px;
+	}
+
+	@media only screen and (min-width: 768px) {
+		height: ${props => (props.size === 'small' ? '200px' : '300px')};
 	}
 `
