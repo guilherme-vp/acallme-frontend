fc7262307fcecec494475caeb4851e344378a829
Guilherme Vieira <guivpw68@gmail.com>
2021-10-14 14:46:50 -0300

    feat: add dashboard container and content styles

---
+++ src/layouts/Dashboard/Dashboard.styled.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+import { Grid } from '@mui/material'
+
+export const Container = styled(Grid)`
+	background-color: ${props => props.theme.background.main};
+	min-height: 100vh;
+
+	@media only screen and (min-width: 768px) {
+		padding: 0 60px;
+	}
+`
+
+export const Content = styled(Grid)`
+	padding: 60px 15px;
+	border-radius: 0;
+	margin: 0;
+
+	@media only screen and (min-width: 768px) {
+		border-radius: 10px;
+		padding: 0px 20px;
+		min-height: 100%;
+	}
+`
