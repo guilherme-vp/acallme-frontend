03ef585a90bbb78ae1a35bf5f7a0fde9b5cbb22c
Guilherme Vieira <guivpw68@gmail.com>
2021-10-14 18:52:31 -0300

    feat: add drawer settings

--- src/state/general.ts
+++ src/state/general.ts
@@ -4,13 +4,16 @@ import { Locales } from '../locales'
 
 export interface GeneralEvents {
 	'language/change': Locales
+	'drawer/set': boolean
 }
 
 export interface GeneralState {
 	language: Locales
+	expanded: boolean
 }
 
 export const generalModule: StoreonModule<GeneralState, GeneralEvents> = store => {
-	store.on('@init', () => ({ language: 'pt-BR' }))
+	store.on('@init', () => ({ language: 'pt-BR', expanded: false }))
 	store.on('language/change', (_state, choosenLanguage) => ({ language: choosenLanguage }))
+	store.on('drawer/set', (_state, value) => ({ expanded: value }))
 }
