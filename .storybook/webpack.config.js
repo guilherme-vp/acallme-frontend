9f55e5c196ec91166b4457fc3de81880daa8adea
Guilherme Vieira <guivpw68@gmail.com>
2021-10-15 17:27:39 -0300

    chore: add material ui styled engine sc alias for webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = async ({ config }) => {
+	config.resolve.alias['@mui/styled-engine'] = '@mui/styled-engine-sc'
 	// Make whatever fine-grained changes you need
 
 	// Return the altered config
--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,8 @@
-const { override, useBabelRc } = require('customize-cra')
+const { addWebpackAlias, override, useBabelRc } = require('customize-cra')
 
-module.exports = override(useBabelRc())
+module.exports = override(
+	addWebpackAlias({
+		'@mui/styled-engine': '@mui/styled-engine-sc'
+	}),
+	useBabelRc()
+)
