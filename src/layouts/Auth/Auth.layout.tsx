7642c65ffe9ffbc7421ca025e2d171b7a3838062
Guilherme Vieira <guivpw68@gmail.com>
2021-10-15 00:44:25 -0300

    feat: add user redirect on logged

--- src/layouts/Auth/Auth.layout.tsx
+++ src/layouts/Auth/Auth.layout.tsx
@@ -1,28 +1,39 @@
 import React from 'react'
 
 import { Grid, Toolbar } from '@mui/material'
+import { Redirect } from 'react-router'
 
+import { useStoreon } from 'hooks'
 import { Copyright } from 'parts/Copyright'
 import { HeaderTitle } from 'parts/Header'
+import { DASHBOARD } from 'routes'
 
 import * as S from './Auth.styled'
 
-export const AuthLayout: React.FC = ({ children }) => (
-	<S.Container
-		container
-		alignItems="center"
-		justifyContent="space-between"
-		flexDirection="column"
-	>
-		<Grid item>
-			<HeaderTitle />
-			<Toolbar sx={{ height: '77px' }} />
-		</Grid>
-		<S.Content container>{children}</S.Content>
-		<Grid container flexDirection="column" alignItems="center">
-			<Copyright />
-		</Grid>
-	</S.Container>
-)
+export const AuthLayout: React.FC = ({ children }) => {
+	const { token } = useStoreon('token')
+
+	if (token) {
+		return <Redirect to={DASHBOARD} />
+	}
+
+	return (
+		<S.Container
+			container
+			alignItems="center"
+			justifyContent="space-between"
+			flexDirection="column"
+		>
+			<Grid item>
+				<HeaderTitle />
+				<Toolbar sx={{ height: '77px' }} />
+			</Grid>
+			<S.Content container>{children}</S.Content>
+			<Grid container flexDirection="column" alignItems="center">
+				<Copyright />
+			</Grid>
+		</S.Container>
+	)
+}
 
 export default AuthLayout
