4fcf311041dc6fc43e75983d4332c7453371b4c6
Guilherme Vieira <guivpw68@gmail.com>
2021-10-15 02:33:48 -0300

    feat: add specialist card style components

---
+++ src/components/SpecialistCard/SpecialistCard.styled.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+import { Button, Paper, Avatar } from '@mui/material'
+
+export const CardContainer = styled(Paper).attrs({ elevation: 0 })`
+	padding: 16px;
+	display: flex;
+	align-items: center;
+	max-width: 350px;
+	box-shadow: -2px 4px 6px 0 rgba(0, 0, 0, 0.1) !important;
+	border-radius: 12px;
+`
+
+export const BookButton = styled(Button).attrs({
+	sx: {
+		borderRadius: '24px',
+		fontWeight: 600,
+		paddingRight: '16px',
+		paddingLeft: '16px',
+		textTransform: 'capitalize'
+	}
+})`
+	margin-top: 8px;
+
+	@media only screen and (min-width: 768px) {
+		margin-top: 0;
+	}
+`
+
+export const MiniAvatar = styled(Avatar).attrs({ sx: { width: 56, height: 56 } })`
+	border: 4px solid ${props => props.theme.tags.white};
+	box-shadow: -2px 4px 6px 0 rgba(0, 0, 0, 0.2);
+`
