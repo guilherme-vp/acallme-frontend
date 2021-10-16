fe84db6cca1330e305b37f732958b6aa2edae20d
Guilherme Vieira <guivpw68@gmail.com>
2021-10-15 22:24:44 -0300

    fix: remove box shadow and add border

--- src/components/SearchInput/SearchInput.style.tsx
+++ src/components/SearchInput/SearchInput.style.tsx
@@ -10,8 +10,9 @@ export const SearchContainer = styled(Paper)`
 	padding: 5.5px 12px;
 	border-radius: 8px;
 	background-color: ${props => alpha(props.theme.grey.fourth, 0.6)};
-	border: 1px solid ${props => props.theme.border.thin};
-	color: ${props => props.theme.text.description};
+	border: 1px solid ${props => props.theme.border.darker};
+	color: ${props => props.theme.text.main};
+	box-shadow: none !important;
 
 	i {
 		color: ${props => props.theme.text.description};
@@ -21,7 +22,7 @@ export const SearchContainer = styled(Paper)`
 export const SearchInputBase = styled(InputBase).attrs({
 	fullWidth: true
 })`
-	margin-left: 10px;
+	margin: 0 4px;
 	font-size: 14px;
 	color: ${props => props.theme.text.description};
 `
