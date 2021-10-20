0ebd4bc20a55bbfa9641d2fbd4172d22e837ebe0
Guilherme Vieira <guivpw68@gmail.com>
2021-10-20 16:19:21 -0300

    feat: add ws events

---
+++ src/constants/ws-events.ts
@@ -0,0 +1,12 @@
+export enum WsEvents {
+	ME = 'me',
+	SEND_MESSAGE = 'send-message',
+	RECEIVE_MESSAGE = 'receive-message',
+	UPDATE_MEDIA = 'update-media',
+	UPDATE_USER_MEDIA = 'update-user-media',
+	ENTER_CALL = 'enter-call',
+	END_CALL = 'end-call',
+	RECEIVE_USER = 'receive-user',
+	SEND_NOTIFICATION = 'send-notification',
+	SEND_CLOSE_NOTIFICATION = 'send-close-notification'
+}
