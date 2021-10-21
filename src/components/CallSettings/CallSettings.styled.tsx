8b637ff8071057d2c3839c7c681f20defb547a10
Guilherme Vieira <guivpw68@gmail.com>
2021-10-21 01:54:48 -0300

    feat: add option icon button styled component

--- src/components/CallSettings/CallSettings.styled.tsx
+++ src/components/CallSettings/CallSettings.styled.tsx
@@ -1,16 +1,32 @@
+/* eslint-disable indent */
 import styled from 'styled-components'
 
-import { Box } from '@mui/material'
+import { Box, IconButton } from '@mui/material'
 
 export const SettingsContainer = styled(Box)`
-	background-color: ${props => props.theme.background.disabled};
+	background-color: ${props => props.theme.background.videocall};
 	display: flex;
-	flex-direction: column;
 	align-items: center;
-	justify-content: center;
-	opacity: 0.7;
+	justify-content: space-between;
 	width: 100%;
-	max-width: 500px;
-	padding: 12px 24px;
-	border-radius: 24px;
+	padding: 8px 16px;
+`
+
+export const OptionButton = styled(IconButton).withConfig<{
+	isHangout?: boolean
+	isOff?: boolean
+}>({
+	shouldForwardProp: props => !['isHangout', 'isOff'].includes(props)
+})`
+	background-color: ${({ theme, isHangout, isOff }) =>
+		isHangout || isOff ? theme.tags.red : theme.grey.fifth};
+	color: ${props => props.theme.tags.white};
+
+	${({ isHangout }) =>
+		isHangout && { paddingLeft: '16px', paddingRight: '16px', borderRadius: '24px' }}
+
+	&:hover {
+		background-color: ${({ theme, isHangout, isOff }) =>
+			isHangout || isOff ? theme.tags.red : theme.grey.fifth};
+	}
 `
