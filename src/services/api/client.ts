d3ad24b2d1ce7e79b813cda1368403086c924e4d
Guilherme Vieira <guivpw68@gmail.com>
2021-10-21 18:52:55 -0300

    feat: add auth token handling and status validation

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -1,9 +1,48 @@
 import { Axios } from 'axios'
+import iziToast from 'izitoast'
 import { QueryClient } from 'react-query'
 
 import { apiUrls } from 'config/api'
 
-export const queryClient = new QueryClient()
+const persistedState = localStorage.getItem('storeon')
 
-export const nodeApi = new Axios({ baseURL: apiUrls.node, timeout: 10000 })
-export const javaApi = new Axios({ baseURL: apiUrls.java, timeout: 10000 })
+let token
+
+if (persistedState) {
+	token = JSON.parse(persistedState).token
+}
+
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		mutations: {
+			onError: (error: any) => {
+				const errorData = JSON.parse(error.response.data)
+
+				if (errorData.error) {
+					iziToast.error({ message: errorData.error })
+				} else {
+					iziToast.error({ message: 'Um erro ocorreu, tente novamente.' })
+				}
+			}
+		}
+	}
+})
+
+export const nodeApi = new Axios({
+	baseURL: apiUrls.node,
+	timeout: 5000,
+	headers: {
+		'Content-type': 'application/json',
+		authorization: token ? `Bearer ${token}` : ''
+	},
+	validateStatus: status => status >= 200 && status < 400
+})
+export const javaApi = new Axios({
+	baseURL: apiUrls.java,
+	timeout: 5000,
+	headers: {
+		'Content-type': 'application/json',
+		authorization: token ? `Bearer ${token}` : ''
+	},
+	validateStatus: status => status >= 200 && status < 400
+})
