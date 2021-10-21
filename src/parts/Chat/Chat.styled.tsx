e9160bde9e77dc401e548a6cce8d16d46f420371
Guilherme Vieira <guivpw68@gmail.com>
2021-10-21 02:02:04 -0300

    feat: add chat header and change height strategy

--- src/parts/Chat/Chat.styled.tsx
+++ src/parts/Chat/Chat.styled.tsx
@@ -2,20 +2,31 @@ import styled from 'styled-components'
 
 import { Box } from '@mui/material'
 
+export const Header = styled(Box)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 4px 16px 8px;
+`
+
 export const ChatContainer = styled.main`
+	display: flex;
 	height: 100%;
-	flex: 1 1 70%;
+	flex: 1;
+	flex-direction: column;
+	padding: 16px;
+	border-radius: 16px;
+	background-color: ${props => props.theme.background.light};
 `
 
 export const MessageWrapper = styled(Box)`
-	max-height: 600px;
+	max-height: max-content;
 	overflow-y: scroll;
-	border: 1px solid ${props => props.theme.border.darker};
 	border-radius: 14px;
 	margin-bottom: 16px;
-	min-height: 600px;
-	padding-top: 24px;
-	padding-bottom: 24px;
+	padding-top: 16px;
+	padding-bottom: 16px;
+	height: 100%;
 `
 
 export const SenderContainer = styled(Box)`
