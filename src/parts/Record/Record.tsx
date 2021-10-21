a506bb38bbb47388e8ba29e53afa83ae9c2bdd2f
Guilherme Vieira <guivpw68@gmail.com>
2021-10-21 05:16:59 -0300

    feat: add record component

---
+++ src/parts/Record/Record.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect } from 'react'
+
+import { Divider, Slide, Typography, IconButton, Grid, TextField } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import { MdClose as CloseIcon, MdLocalHospital as DiagnosisIcon } from 'react-icons/md'
+
+import { InputIconContainer } from 'components/InputAdornment'
+import { useIntl } from 'hooks'
+import { Record as RecordEntity } from 'services/entities'
+
+import * as S from './Record.styled'
+
+export type FormProps = Pick<RecordEntity, 'diagnosis' | 'observation'>
+
+export interface RecordProps {
+	open: boolean
+	callEnded: boolean
+	onSubmit: (data: FormProps) => void
+	handleClose: () => void
+}
+
+export const Record = ({ open, handleClose, callEnded, onSubmit }: RecordProps) => {
+	const intl = useIntl()
+	const { register, formState, getValues } = useForm<FormProps>({ mode: 'all' })
+
+	const { errors } = formState
+
+	useEffect(() => {
+		const formData = getValues()
+
+		onSubmit(formData)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [callEnded])
+
+	return (
+		<Slide direction="up" in={open}>
+			<S.RecordContainer>
+				<S.Header>
+					<Typography variant="body1">
+						{intl.formatMessage({ id: 'record.title' })}
+					</Typography>
+					<IconButton onClick={() => handleClose()} sx={{ fontSize: 18 }}>
+						<CloseIcon />
+					</IconButton>
+				</S.Header>
+				<Divider />
+				<S.RecordWrapper>
+					<Grid container alignItems="stretch" spacing={3}>
+						<Grid item xs={12}>
+							<Typography variant="body1" textAlign="center" color="text.secondary">
+								{intl.formatMessage({ id: 'record.description' })}
+							</Typography>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								{...register('diagnosis', { required: true, minLength: 1 })}
+								fullWidth
+								label={intl.formatMessage({ id: 'record.diagnosis' })}
+								variant="outlined"
+								required
+								error={!!errors.diagnosis}
+								name="diagnosis"
+								placeholder={intl.formatMessage({ id: 'record.diagnosis.placeholder' })}
+								InputProps={{
+									startAdornment: (
+										<InputIconContainer position="start">
+											<DiagnosisIcon />
+										</InputIconContainer>
+									)
+								}}
+							/>
+						</Grid>
+						<Grid item xs={12} sx={{ marginTop: '16px' }}>
+							<TextField
+								{...register('observation')}
+								fullWidth
+								label={intl.formatMessage({ id: 'record.observation' })}
+								variant="outlined"
+								error={!!errors.observation}
+								name="observation"
+								multiline
+								rows={10}
+								placeholder={intl.formatMessage({ id: 'record.observation.placeholder' })}
+							/>
+						</Grid>
+					</Grid>
+				</S.RecordWrapper>
+			</S.RecordContainer>
+		</Slide>
+	)
+}
+
+export default Record
