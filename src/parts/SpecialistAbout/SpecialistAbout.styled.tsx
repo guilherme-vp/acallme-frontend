bc325565fab5e1eb3a8b256a168c24c7e58740f5
Guilherme Vieira <guivpw68@gmail.com>
2021-10-22 03:54:08 -0300

    feat: add specialist about styled components

---
+++ src/parts/SpecialistAbout/SpecialistAbout.styled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import { Avatar } from '@mui/material'
+
+export const BigAvatar = styled(Avatar)`
+	width: 80px;
+	height: 80px;
+`
+
+export const MainContent = styled.main`
+	width: 100%;
+	min-height: 100%;
+`
