59623dc609162865b60f156e7fbd3894d2519ca9
Guilherme Vieira <guivpw68@gmail.com>
2021-10-22 16:58:57 -0300

    refactor: use use effect to update weeks

--- src/hooks/useSchedule.ts
+++ src/hooks/useSchedule.ts
@@ -1,13 +1,12 @@
 /* eslint-disable import/no-duplicates */
+import { useState, useEffect } from 'react'
+
 import { format, startOfWeek, daysInWeek, addDays, set, endOfWeek } from 'date-fns'
 import { enUS, ptBR } from 'date-fns/locale'
-import { useQuery } from 'react-query'
 
-import { fetchSchedules } from 'services/api/schedule'
-import { RolesEnum } from 'services/entities'
+import { Schedule } from 'services/entities'
 
 import { useIntl } from './useIntl'
-import { useStoreon } from './useStoreon'
 
 export interface HoursRange {
 	day: Date
@@ -33,11 +32,13 @@ export interface ScheduleContent {
 	schedule: WeekContent
 }
 
-const dayStart = 6
-const dayEnds = 21
+export const dayStart = 6
+export const dayEnds = 21
 
-export const useSchedule = (date: Date = new Date()) => {
-	const { user, role } = useStoreon('user', 'role')
+export const useSchedule = (
+	data: Schedule[] | undefined,
+	date: Date = new Date()
+): ScheduleContent => {
 	const intl = useIntl()
 
 	function formatDay(oldDate: Date) {
@@ -48,88 +49,91 @@ export const useSchedule = (date: Date = new Date()) => {
 	const lastDay = endOfWeek(date, { weekStartsOn: 1 })
 	const startWeekMonth = format(firstDay, 'MMM')
 	const year = format(date, 'yyyy')
-
-	const { data } = useQuery('schedules', () =>
-		fetchSchedules({
-			specialistId: role === RolesEnum.Specialist && user ? user.id : undefined,
-			patientId: role === RolesEnum.Specialist && user ? user.id : undefined,
-			rangeStart: set(firstDay, { hours: dayStart }).toISOString(),
-			rangeEnd: set(lastDay, { hours: dayEnds }).toISOString()
-		})
-	)
-
 	const firstDayOfWeek = startOfWeek(date, { weekStartsOn: 1 })
 
 	const intlLocale = intl.locale === 'pt-BR' ? ptBR : enUS
 
-	const allFormattedHours: HoursRange[][] = Array(dayEnds - dayStart) // 21h - 6h = 15h
-		.fill(null)
-		.map((_, index) => {
-			const hour = index + dayStart // Day start at 6am
+	function getWeekDays() {
+		const allFormattedHours: HoursRange[][] = Array(dayEnds - dayStart) // 21h - 6h = 15h
+			.fill(null)
+			.map((_, index) => {
+				const hour = index + dayStart // Day start at 6am
 
-			const mockedWeekDays: HoursRange[] = Array(daysInWeek)
-				.fill(null)
-				.map((__, day): HoursRange => {
-					const aimedDay = set(addDays(firstDayOfWeek, day), { hours: hour, minutes: 0 })
+				const mockedWeekDays: HoursRange[] = Array(daysInWeek)
+					.fill(null)
+					.map((__, day): HoursRange => {
+						const aimedDay = set(addDays(firstDayOfWeek, day), {
+							hours: hour,
+							minutes: 0
+						})
 
-					const formattedHour = format(aimedDay, 'HH:mm')
+						const formattedHour = format(aimedDay, 'HH:mm')
 
-					const scheduleExists = data?.find(
-						({ rangeStart }) => rangeStart.getTime() === aimedDay.getTime()
-					)
+						const scheduleExists = data?.find(
+							({ rangeStart }) => new Date(rangeStart).getTime() === aimedDay.getTime()
+						)
 
-					if (!scheduleExists) {
+						if (!scheduleExists) {
+							return {
+								day: aimedDay,
+								hour: formattedHour,
+								isScheduled: false,
+								isDisabled: false
+							}
+						}
+
 						return {
 							day: aimedDay,
 							hour: formattedHour,
-							isScheduled: false,
-							isDisabled: false
+							isScheduled: !!scheduleExists.patientId,
+							isDisabled: scheduleExists.disabled,
+							isConfirmed: scheduleExists.confirmed,
+							scheduleId: scheduleExists.id
 						}
-					}
+					})
+					.sort((a, b) => (a.day > b.day ? 1 : -1))
 
-					return {
-						day: aimedDay,
-						hour: formattedHour,
-						isScheduled: !!scheduleExists.patientId,
-						isDisabled: scheduleExists.disabled,
-						isConfirmed: scheduleExists.confirmed,
-						scheduleId: scheduleExists.id
-					}
-				})
-				.sort((a, b) => (a.day > b.day ? 1 : -1))
+				return mockedWeekDays
+			}) as HoursRange[][]
 
-			return mockedWeekDays
-		}) as HoursRange[][]
+		const allFormattedWeeks: WeekHeader[] = Array(daysInWeek)
+			.fill(null)
+			.map((_, index) => {
+				const aimedDay = addDays(firstDayOfWeek, index)
 
-	const allFormattedWeeks: WeekHeader[] = Array(daysInWeek)
-		.fill(null)
-		.map((_, index) => {
-			const aimedDay = addDays(firstDayOfWeek, index)
+				const desc = format(aimedDay, 'LLL do')
 
-			const desc = format(aimedDay, 'LLL do')
+				return {
+					title: format(aimedDay, 'E'),
+					desc
+				}
+			})
 
-			return {
-				title: format(aimedDay, 'E'),
-				desc
-			}
-		})
+		const allWeek = { hours: allFormattedHours, weeks: allFormattedWeeks }
 
-	const allWeek = { hours: allFormattedHours, weeks: allFormattedWeeks }
+		let selector: string
 
-	let selector: string
+		if (+formatDay(firstDay) > +formatDay(lastDay)) {
+			const nextMonth = format(lastDay, 'MMM', { locale: intlLocale })
 
-	if (+formatDay(firstDay) > +formatDay(lastDay)) {
-		const nextMonth = format(lastDay, 'MMM', { locale: intlLocale })
+			const formattedfirstDay = formatDay(firstDay)
+			const formattedLastDay = formatDay(lastDay)
 
-		const formattedfirstDay = formatDay(firstDay)
-		const formattedLastDay = formatDay(lastDay)
+			selector = `${startWeekMonth} ${formattedfirstDay} - ${nextMonth} ${formattedLastDay}, ${year}`
+		} else {
+			selector = `${startWeekMonth} ${formatDay(firstDay)}-${formatDay(lastDay)}, ${year}`
+		}
 
-		selector = `${startWeekMonth} ${formattedfirstDay} - ${nextMonth} ${formattedLastDay}, ${year}`
-	} else {
-		selector = `${startWeekMonth} ${formatDay(firstDay)}-${formatDay(lastDay)}, ${year}`
+		return { selector, schedule: allWeek }
 	}
+	const [week, setWeek] = useState<ScheduleContent>(getWeekDays())
 
-	return { selector, schedule: allWeek }
+	useEffect(() => {
+		setWeek(getWeekDays())
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [data, date])
+
+	return week
 }
 
 export default useSchedule
