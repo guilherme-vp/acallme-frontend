60bcbe18bfa0d46e67a22d8c87e0d40c20128d51
Guilherme Vieira <guivpw68@gmail.com>
2021-10-22 19:00:47 -0300

    fix: change colors

--- src/components/BookButton/BookButton.styled.tsx
+++ src/components/BookButton/BookButton.styled.tsx
@@ -5,19 +5,22 @@ import { Button } from '@mui/material'
 export const BookButton = styled(Button).withConfig<{
 	isScheduled: boolean
 	isConfirmed?: boolean
+	isChosen?: boolean
 }>({
-	shouldForwardProp: props => !['isScheduled', 'isConfirmed'].includes(props)
+	shouldForwardProp: props => !['isScheduled', 'isConfirmed', 'isChosen'].includes(props)
 })`
-	background-color: ${({ disabled, isScheduled, isConfirmed, theme }) =>
+	background-color: ${({ disabled, isScheduled, isConfirmed, isChosen, theme }) =>
 		disabled
 			? 'transparent'
 			: isScheduled && !isConfirmed
 			? theme.colors.alternative
 			: isScheduled && isConfirmed
 			? theme.colors.secondary
+			: isChosen
+			? theme.colors.primary
 			: 'transparent'} !important;
-	color: ${({ isScheduled, theme }) =>
-		!isScheduled ? theme.text.description : theme.tags.white};
+	color: ${({ isScheduled, isChosen, theme }) =>
+		!isScheduled && !isChosen ? theme.text.description : theme.tags.white};
 	padding: 8px 30px;
 	border-radius: 8px;
 	font-weight: 600;
